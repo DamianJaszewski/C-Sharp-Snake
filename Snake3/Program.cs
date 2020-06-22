9a1ef9e83a719ed4060de75ef4c79a3c86fe3d00
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2020-06-22 22:37:45 +0200

    Dodaj pliki projektów.

---
+++ Snake3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake3
+{
+    class Snake
+    {
+        int Height = 20; //wysokosc planszy
+        int Width = 30; //szerokosc planszy
+        public void WriteBoard()
+        {
+            Console.Clear();
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Height + 2));
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("|");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition((Width + 2), i);
+                Console.Write("|");
+            }
+
+            Console.WriteLine("This is my first game!");
+            Console.ReadKey();
+
+        }
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+
+            snake.WriteBoard();
+
+        }   
+    }
+}
